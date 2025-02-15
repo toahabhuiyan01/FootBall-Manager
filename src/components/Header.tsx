1c4c7e756a0386081ce484a48c2f286250d6a34d
Toaha Bhuiyan <67577253+toahabhuiyan01@users.noreply.github.com>
2025-02-15 13:51:06 +0600

    chore: devide the APP component

---
+++ src/components/Header.tsx
@@ -0,0 +1,70 @@
+import {
+    AppBar,
+    Button,
+    Toolbar,
+    Typography,
+    TextField,
+    IconButton,
+    InputAdornment,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+
+interface HeaderProps {
+    searchQuery: string;
+    onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Header({ searchQuery, onSearchChange }: HeaderProps) {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    sx={{ mr: 2 }}
+                >
+                    <SportsSoccerIcon />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ flexGrow: 0, mr: 4 }}
+                >
+                    Football Manager
+                </Typography>
+                <TextField
+                    size="small"
+                    placeholder="Search players..."
+                    value={searchQuery}
+                    onChange={onSearchChange}
+                    sx={{
+                        flexGrow: 1,
+                        mr: 2,
+                        "& .MuiInputBase-root": {
+                            backgroundColor: "rgba(255, 255, 255, 0.15)",
+                            color: "white",
+                            "&:hover": {
+                                backgroundColor: "rgba(255, 255, 255, 0.25)",
+                            },
+                        },
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: "rgba(255, 255, 255, 0.3)",
+                        },
+                    }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <SearchIcon sx={{ color: "white" }} />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button variant="contained" color="secondary" sx={{ ml: 2 }}>
+                    Create Team
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
